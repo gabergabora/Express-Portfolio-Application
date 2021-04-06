3b297ff0ab4e06746801fb927fba412ee187af0c
George <angelov2000@abv.bg>
2021-04-06 11:35:17 +0300

    crud operations

--- Portfolio Application/routes/admin.js
+++ Portfolio Application/routes/admin.js
@@ -13,14 +13,13 @@ router.get("/add", (req, res, next) => {
   });
 });
 
-router.post("/add",(req, res, next) => {
+router.post("/add", (req, res, next) => {
   const title = req.body.title;
   const description = req.body.description;
   const service = req.body.service;
   const client = req.body.client;
   const projectUrl = req.body.projectUrl;
   const date = new Date();
-
 
   body("title", "Title field is required").notEmpty();
   body("description", "Description field is required").notEmpty();
@@ -31,11 +30,6 @@ router.post("/add",(req, res, next) => {
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    /*
-        res.render('admin/add',{
-            errors: errors,
-        });
-        */
   } else {
     const courses = db.get("courses");
     courses.insert(
@@ -60,11 +54,70 @@ router.post("/add",(req, res, next) => {
 
 router.get("/show/:id", (req, res, next) => {
   const courses = db.get("courses");
-
   courses.findOne(req.params.id, (error, course) => {
     res.render("admin/details", {
       course: course,
     });
   });
 });
+
+router.get("/edit/:id", (req, res, next) => {
+  const courses = db.get("courses");
+  courses.findOne(req.params.id, (error, course) => {
+    res.render("admin/edit", {
+      course: course,
+    });
+  });
+});
+
+router.post("/edit/:id", (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const service = req.body.service;
+  const client = req.body.client;
+  const projectUrl = req.body.projectUrl;
+
+  body("title", "Title field is required").notEmpty();
+  body("description", "Description field is required").notEmpty();
+  body("service", "Service field is required").notEmpty();
+  body("client", "Client field is required").notEmpty();
+  body("projectUrl", "projectUrl field is required").notEmpty();
+
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+  } else {
+    const courses = db.get("courses");
+    courses.updateOne(
+      {
+        title: title,
+        description: description,
+        service: service,
+        client: client,
+        projectUrl: projectUrl,
+      },
+      (error, post) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.redirect("/");
+        }
+      }
+    );
+  }
+});
+
+router.delete("/delete/:id", (req, res) => {
+  const courses = db.get("courses");
+  courses.findByIdAndDelete(req.params.id,(error,docs)=>{
+    if(error){
+      console.log(error)
+    }else{
+      console.log(docs)
+    }
+  })
+    
+});
+
 module.exports = router;
