a6d3a2191bd54e64b4392fb8aacc257ff3b047f9
George <angelov2000@abv.bg>
2021-04-07 13:28:20 +0300

    Completed

---
+++ Portfolio Application/models/courses.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+const coursesSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    service:{
+        type:String,
+        required:true,
+    },
+    client:{
+        type:String,
+        required:true,
+    },
+    projectUrl:{
+        type:String,
+        required:true,
+    },
+    mainimage:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+});
+module.exports=mongoose.model('Courses',coursesSchema)
